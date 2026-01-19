0492cf3d2e185215db51a83e62298362e2e75209
Oriana <98231498+OrianaLand@users.noreply.github.com>
2026-01-19 20:53:17 -0300

    Add knightMoves pathfinding function

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ import Knight from "./knight.js";
 import Board from "./board.js";
 import { bfs } from "./search.js";
 
-let knight = new Knight();
-let moves = knight.getPossibleMoves([1, 1]);
-console.log(moves);
-
-let board = new Board();
-moves.forEach((move) => console.log(board.isValidPosition(move)));
-
-function getNeighbors(position) {
-  const possibleMoves = knight.getPossibleMoves(position);
-  return possibleMoves.filter((move) => board.isValidPosition(move));
+function createGetNeighbors(knight, board) {
+  //get the actual valid moves
+  return function getNeighbors(position) {
+    const moves = knight.getPossibleMoves(position);
+    return moves.filter((move) => board.isValidPosition(move));
+  };
 }
-
-const neighbors = getNeighbors([0, 0]);
-console.log(neighbors);
 
 function knightMoves(start, end) {
-  // 1. create board
-  // 2. create knight
-  // 3. define how to get valid neighbors
-  // 4. run bfs
-  // 5. return or print path
+  const board = new Board();
+  const knight = new Knight();
+  const getNeighbors = createGetNeighbors(knight, board);
+  const path = bfs(start, end, getNeighbors);
+
+  console.log(`You made it in ${path.length - 1} moves!`);
+  path.forEach((square) => console.log(square));
+
+  return path;
 }
 
-// Example usage (later):
-// knightMoves([0, 0], [7, 7]);
+knightMoves([0, 0], [7, 7]);
